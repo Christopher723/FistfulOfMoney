96dcc1c4bf96e87626b334bc554f4ab8a22af9bf
Christopher <Christopher.woods723@gmail.com>
2023-10-18 15:46:46 -0400

    test

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -7,5 +7,5 @@ import Foundation
 
 func chapterFour() {
     // Your portion of the story goes here
-    print("they all end up happily ever after")
+    print("they all end up happily ever after test")
 }
