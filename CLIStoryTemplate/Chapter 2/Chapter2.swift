b6a4a088f1ca103c65692bf2ffbe81698d11ea94
Cleo Larkin <clarkin23@msu.idserve.net>
2023-10-18 15:30:01 -0400

    cleo story

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -7,4 +7,6 @@ import Foundation
 
 func chapterTwo() {
     // Your portion of the story goes here
+    
+    print("Our hero sees a towny gettin beatup by some thugs then intervieens with an axe handle to save the day. Now our hero is a real hero to all the townies. Hero then is asked to join in all the other fights. In the mean time we are having Bacon, Lettuce tomato sandwiches on toast with mayonaise.")
 }
