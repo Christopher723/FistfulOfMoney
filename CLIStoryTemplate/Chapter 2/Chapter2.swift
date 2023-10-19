8738d7f98ceb6bd01097c0c72395fd65c929e8f3
Cleo Larkin <clarkin23@msu.idserve.net>
2023-10-19 15:36:26 -0400

    added variables

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -8,5 +8,10 @@ import Foundation
 func chapterTwo() {
     // Your portion of the story goes here
     
-    print("Our hero sees a towny gettin beatup by some thugs then intervieens with an axe handle to save the day. Now our hero is a real hero to all the townies. Hero then is asked to join in all the other fights. In the mean time we are having Bacon, Lettuce tomato sandwiches on toast with mayonaise.")
+    var some = 3
+    var an = 1
+    var all = 100
+    
+    print("Our hero sees a towny gettin beatup by \(some) thugs then intervieens with \(an) axe handle to save the day. Now our hero is a real hero to \(all) the townies. Hero then is asked to join in all the other fights. In the mean time we are having Bacon, Lettuce tomato sandwiches on toast with mayonaise.")
 }
+
