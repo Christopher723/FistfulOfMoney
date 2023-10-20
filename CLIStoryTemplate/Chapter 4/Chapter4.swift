0fc7e7bd2643a10c3fabac10caeac9f6da2d99e2
Christopher <Christopher.woods723@gmail.com>
2023-10-20 16:03:13 -0400

    added ending chooser

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -12,7 +12,22 @@ func chapterFour() {
     var problem = "BLTS"
     var myInt = 10
     var pluralNouns =  "BLTs"
-    var amountYears = 42
     
-    print("The main character \(name) finally realised that the key to everyones struggles was the lack of \(problem) he decided to bring in \(myInt) \(pluralNouns) to help everyone out. Afer that the groups lived peacefully for \(amountYears) years")
+    var isHappyEnding: Bool
+    
+    print("Is this a happy ending story? (Y or N)")
+    if var temp = readLine()?.lowercased(){
+        
+        if temp == "y"{
+            isHappyEnding = true
+        }
+        else {isHappyEnding = false}
+            
+            
+            
+        print("The main character \(name) finally realised that the key to everyones struggles was the lack of \(problem) he decided to bring in \(myInt) \(pluralNouns) to help everyone out.\(isHappyEnding ? "Afer that the groups lived peacefully for the rest of time" : "Unfortunately the food was moldy and everyone died and the problem was solved in the worse way possible")"
+                  )
+    }
+    
 }
+
