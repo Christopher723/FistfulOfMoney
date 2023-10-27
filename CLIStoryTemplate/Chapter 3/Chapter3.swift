acba69b4fca44c939ff1a6a80a85cd40e07fad7e
Tyler Fantow <tylerfantow7@gmail.com>
2023-10-27 15:31:16 -0400

    new chap 3

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -4,7 +4,17 @@
 //
 
 import Foundation
+let character = "hero"
+   let atagonist = "bad guy"
+   var needsHelp = true
+   var problem = true
+   var Bacon = 1
+   var lettuce = 1
+   var tomato = 1
+   var toast = 1
+   var mayo = 1
 
 func chapterThree() {
-    print("I'm tyler and wrinting chapter three test")
+   
+    
 }
