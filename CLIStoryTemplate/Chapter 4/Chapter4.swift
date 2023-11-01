68e0e79eedf65051a3a8c493e0855b65971fe8ff
Christopher <Christopher.woods723@gmail.com>
2023-11-01 16:47:33 -0400

    added my chapter

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -83,8 +83,9 @@ func chapterFour() {
 ////            isHappyEnding = true
 ////        }
 ////        else {isHappyEnding = false}
-//        
-//        
-//        
+//
+//
+//
 ////        print(" \(MainC)The main character finally realised that the key to everyones struggles was the lack of \(problem) he decided to bring in \(myInt) \(pluralNouns) to help everyone out.\(isHappyEnding ? "Afer that the groups lived peacefully for the rest of time" : "Unfortunately the food was moldy and everyone died and the problem was solved in the worse way possible")"
 //                  )
+
