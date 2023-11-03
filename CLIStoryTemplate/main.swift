e48fc510b036d3cb99728ba75e74202628ece912
Cleo Larkin <clarkin23@msu.idserve.net>
2023-11-03 14:19:26 -0400

    upgrade

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -1,7 +1,8 @@
 //
 //  main.swift
 //  CLIStoryTemplate
-//
+//  This story is about a man named the High Plains Drifter a movie of the spagetti western ers 1965.
+//  The hero (Clint Eastwood) became famous for this role as a gunslinging drifter. These are some of his adventures.
 
 import Foundation
 
