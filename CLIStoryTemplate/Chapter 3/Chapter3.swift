f26d13d4df758a6c2dac3e3b57616e3289688453
Tyler Fantow <tylerfantow7@gmail.com>
2023-11-07 13:44:39 -0500

    newest version

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -4,17 +4,44 @@
 //
 
 import Foundation
-let character = "hero"
-   let atagonist = "bad guy"
-   var needsHelp = true
-   var problem = true
-   var Bacon = 1
-   var lettuce = 1
-   var tomato = 1
-   var toast = 1
-   var mayo = 1
+
+struct townResident {
+    var indeistress: Bool
+    var name: String
+    var problem: String?
+    var solution: String?
+    init(indeistress: Bool, name: String, problem: String, solution: String) {
+        self.indeistress = indeistress
+        self.name = name
+        self.problem = problem
+        self.solution = solution
+    }
+}
 
 func chapterThree() {
-   
+    let hero = "Clint"
+    var townie1 = townResident(indeistress: true, name: "George", problem: "my son is sick and the doctor doesnt have the medicine he needs and the next closest place is days away.", solution: "goes and gets it")
+    var townie2 = townResident(indeistress: true, name: "Bob", problem: "the thugs that keep robbing my store.", solution: "helps Bob by fighting off the thugs and warning them if they come back he wont be as gentle.")
+    var townie3 = townResident(indeistress: false, name: "Trevor", problem: "", solution: "")
+    var sadArr: [townResident] = []
+    let people =  [townie1, townie2, townie3]
+    print("Our hero \(hero) walks into his next town on his journy across the country. The town looks like it can use his help.")
+    for people in people where people.indeistress == true{
+        sadArr.append(people)
+        
+    }
+    while !sadArr.isEmpty{
+        print(" \(hero) walks up to a town residident seeing him in distress. Please help me my name is \(sadArr[0].name), I need help with \(String(describing: sadArr[0].problem))")
+        print("Our hero then \(String(describing: sadArr[0].solution))")
+        sadArr.remove(at: 0)
+        if sadArr.isEmpty {
+             return print("no idea yet")
+        }
+    }
     
 }
+
+
+
+
+
