5f509495ead26db5fb1fb17469bc170fd82a9661
Mario Swaidan <mario@dronecapital.com>
2023-11-07 13:45:19 -0500

    Chapter 1

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -6,11 +6,25 @@
 import Foundation
 
 func readStory() {
+    print("Fist Full of Money")
+    print("\n")
+    print("By Chris Woods, Tyler Fentow, Cleo Larkins, and Mario Swaidan")
+    print("\n")
+    print("Chapter 1")
     chapterOne()
+    print("\n")
+    print("Chapter 2")
     chapterTwo()
+    print("\n")
+    print("Chapter 3")
     chapterThree()
+    print("\n")
+    print("Chapter 4")
     chapterFour()
+    print("\n")
+    print("Chapter 5")
     chapterFive()
+    print("\n")
 }
 
 readStory()
